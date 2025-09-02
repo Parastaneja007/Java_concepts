c08bbd9f0826754e887659efa394fca1b0199b7a
Paras <ptaneja007@gmail.com>
2025-09-02 16:43:50 +0530

    some changes

--- src/Properties_oops/Car.java
+++ src/Properties_oops/Car.java
@@ -8,7 +8,7 @@ public class Car {
     public Car(){
 
     }
-    public Car(String brand, String color, int model, String type) {
+    public  Car(String brand, String color, int model, String type) {
         this.brand = brand;
         this.color = color;
         this.model = model;
