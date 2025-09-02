4df20e2a7ee3787a369cabeb66bded5142a9bdd8
Paras <ptaneja007@gmail.com>
2025-09-02 16:47:41 +0530

    To tell that upper class can not access lower class data or something

--- src/Properties_oops/Car_run.java
+++ src/Properties_oops/Car_run.java
@@ -6,7 +6,13 @@ public class Car_run {
         Family car2 = new Family("swift" , "white" , 2020 , "petrol" , "automatic");
         System.out.println(car1.type);
         Luxary car3 = new Luxary("Bmw"  , "red" ,2022 , "disel" , "manual" , 4500000);
+        Family car4 = new Family("alto"  , "white" , 2018 , "petrol" , "manual");
         System.out.println(car3.brand);
+//        System.out.println(car4.price);
+        // we can not access price as family is the upper class and it is trying to access the data member
+        //of lower class
+        // lower class can acces the things related to upper class but upper class can not access
+        // from the lower class
 
     }
 }
