e934fc9b5779a01a19bf8ad870ca430265006e63
Paras <ptaneja007@gmail.com>
2025-09-03 23:47:56 +0530

    random files , not much use

---
+++ src/access/Main.java
@@ -0,0 +1,10 @@
+package access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(10 , "paras"  );
+        //access the data members
+        // modify the data member
+
+    }
+}
