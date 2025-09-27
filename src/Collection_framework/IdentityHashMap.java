50a02bf4d6dfd73b6f1473dd07dc9a8bc444c5d4
Paras <ptaneja007@gmail.com>
2025-09-27 13:28:51 +0530

    Identity hash map and working

--- src/Collection_framework/IdentityHashMap.java
+++ src/Collection_framework/IdentityHashMap.java
@@ -8,9 +8,8 @@ public class IdentityHashMap {
         Map<String, Integer> map = new HashMap<>();
         map.put(key1 , 1);// key , 1
         map.put(key2 , 2);//key ,  2
-        //equals meathod-- it will same only one value which is key = 2 , as it will replace it from
-        //existing one but if we use identity hash map then no replace ment will be there 
+        //equals method-- it will same only one value which is key = 2 , as it will replace it from
+        //existing one but if we use identity hash map then no replace ment will be there
         System.out.println(map);
-
       }
     }
