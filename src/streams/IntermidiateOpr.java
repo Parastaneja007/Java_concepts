0637cea9bb587ee0683323e35758dbec4ba11676
Paras <ptaneja007@gmail.com>
2025-10-07 15:47:46 +0530

    intermediate opeations

---
+++ src/streams/IntermidiateOpr.java
@@ -0,0 +1,34 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class IntermidiateOpr {
+    public static void main(String[] args) {
+        // intermediate operation transform a stream into another stream .
+        // they are lazy means they did not execute until a terminal operation is invoked .
+        //1. filter
+        List<String> list = Arrays.asList("paras", "parv", "mukul");
+        Stream<String> filteredStream = list.stream().filter(x -> x.startsWith("p"));
+        // no filtered at above point , no filter until terminal condition
+        //  like count below
+        long res = filteredStream.count();
+        System.out.println(res);
+        // 2. map -- it takes a function means convert data , one data comes and gives another
+        Stream<String> UpperCase = list.stream().map(x -> x.toUpperCase());
+        // 3. sorted -- for natural ordering
+        Stream<String> sorted = list.stream().sorted();
+        list.stream().sorted((a , b) -> a.length() - b.length());
+        // 4 . distinct -- for unique value -- eliminate two words which are same
+        Stream<String> distinct = list.stream().distinct();
+        // 5 . limit
+       Stream.iterate(1 , x-> x+1 ).count(); // this will generate a infinite loop so we can
+        //put limit for particular number we want it to run
+        Stream.iterate(1 , x-> x+1 ).limit(100);
+        // 6 . skip
+        Stream.iterate(1 , x-> x+1 ).skip(10).limit(100);
+        // used to skip some things , in above first 10 are skiped , it will start from 11 and go till
+        // 110 because limit is 100
+    }
+}
